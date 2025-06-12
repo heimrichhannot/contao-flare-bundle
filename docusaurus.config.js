55373e82e49df228a8f458b45f4ea807bf0ae6d9
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-06-12 12:17:22 +0200

    initial docusaurus config

---
+++ docusaurus.config.js
@@ -0,0 +1,134 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Contao Flare Bundle',
+  tagline: 'Filter, List and Reader engine for Contao CMS',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://flare-docs.heimrichhannot.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'heimrichhannot', // Usually your GitHub org/user name.
+  projectName: 'contao-flare-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'de',
+    locales: ['de'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/heimrichhannot/contao-flare-docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/heimrichhannot/contao-flare-docs/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'FLARE',
+        logo: {
+          alt: 'Contao Flare Bundle Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'documentationSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/heimrichhannot/contao-flare-bundle',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Info',
+            items: [
+              {
+                label: 'Heimrich & Hannot GmbH',
+                to: 'https://www.heimrich-hannot.de',
+              },
+              {
+                label: 'Impressum',
+                to: '/imprint',
+              },
+              {
+                label: 'Datenschutzerklärung',
+                to: 'https://www.heimrich-hannot.de/de/datenschutz',
+              },
+            ],
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Heimrich & Hannot GmbH. Alle Rechte vorbehalten.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/pages/imprint.md
@@ -0,0 +1,21 @@
+# Impressum
+
+<address>
+Heimrich & Hannot GmbH
+<br/>An der Frauenkirche 12
+<br/>01067 Dresden
+</address>
+
+**Vertretungsberechtigte Geschäftsführer:**
+<br/>Dr. Walter Hannot, Marco Helas, Bodo Körsten, Carsten Klunker
+
+**Inhaltlich Verantwortlicher gemäß § 18 Abs. 2 MStV:**
+<br/>Dr. Walter Hannot
+
+E-Mail: <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#100;&#x72;&#101;&#x73;&#100;&#x65;&#110;&#x40;&#104;&#x65;&#105;&#x6D;&#114;&#x69;&#99;&#x68;&#45;&#x68;&#97;&#x6E;&#110;&#x6F;&#116;&#x2E;&#100;&#x65;" class="email">&#100;&#x72;&#101;&#x73;&#100;&#x65;&#110;&#x40;&#104;&#x65;&#105;&#x6D;&#114;&#x69;&#99;&#x68;&#45;&#x68;&#97;&#x6E;&#110;&#x6F;&#116;&#x2E;&#100;&#x65;</a>
+<br/>Webseite: <a href="https://www.heimrich-hannot.de">www.heimrich-hannot.de</a>
+
+**Registergericht:** Amtsgericht Dresden
+<br/>**Registernummer:** HR B 10877
+<br/>**Umsatzsteuer-Identifikationsnummer** gemäß § 27a
+Umsatzsteuergesetz: DE 168016090

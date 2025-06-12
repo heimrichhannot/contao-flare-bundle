b8fceb43512e9cd42df1351b890f504d33985706
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-06-12 16:28:43 +0200

    update deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://flare-docs.heimrichhannot.github.io',
+  trailingSlash: false,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',

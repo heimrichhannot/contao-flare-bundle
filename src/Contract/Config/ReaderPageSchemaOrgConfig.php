f4ca47f081a777279b366c1539a54cb03d0519f3
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-09-04 09:17:05 +0200

    declare `ReaderPageSchemaOrgConfig` as `readonly` and update constructor formatting

--- src/Contract/Config/ReaderPageSchemaOrgConfig.php
+++ src/Contract/Config/ReaderPageSchemaOrgConfig.php
@@ -5,12 +5,10 @@ namespace HeimrichHannot\FlareBundle\Contract\Config;
 use Contao\Model;
 use HeimrichHannot\FlareBundle\Model\ListModel;
 
-class ReaderPageSchemaOrgConfig
+readonly class ReaderPageSchemaOrgConfig
 {
     public function __construct(
-        public readonly ListModel $listModel,
-        public readonly Model     $model,
-    )
-    {
-    }
+        public ListModel $listModel,
+        public Model     $model,
+    ) {}
 }

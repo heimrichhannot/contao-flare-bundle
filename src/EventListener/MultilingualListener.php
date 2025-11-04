32825cd646c18cfdc1655ca2bb6b5d0d7a92b21a
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-11-04 14:49:04 +0100

    add listener for changelanguage lang switcher event

--- src/EventListener/MultilingualListener.php
+++ src/EventListener/MultilingualListener.php
@@ -3,16 +3,27 @@
 namespace HeimrichHannot\FlareBundle\EventListener;
 
 use Composer\InstalledVersions;
+use Contao\Controller;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Doctrine\DBAL\Query\QueryBuilder;
 use HeimrichHannot\FlareBundle\Event\CreateListViewBuilderEvent;
 use HeimrichHannot\FlareBundle\ListView\Resolver\MultilingualListViewResolver;
 use HeimrichHannot\FlareBundle\Manager\ListViewManager;
+use HeimrichHannot\FlareBundle\Model\ListModel;
+use HeimrichHannot\FlareBundle\Util\DcMultilingualHelper;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Terminal42\ChangeLanguage\Event\ChangelanguageNavigationEvent;
+use Terminal42\DcMultilingualBundle\Driver;
+use Terminal42\DcMultilingualBundle\QueryBuilder\MultilingualQueryBuilderFactoryInterface;
 
 class MultilingualListener
 {
+    private MultilingualQueryBuilderFactoryInterface $queryBuilderFactory;
 
     public function __construct(
         private readonly ListViewManager $listViewManager,
+        private readonly RequestStack $requestStack,
     ) {
     }
 
@@ -24,5 +35,81 @@ class MultilingualListener
         }
 
         $event->setResolver(new MultilingualListViewResolver($this->listViewManager));
+    }
+
+    #[AsHook('changelanguageNavigation')]
+    public function onChangelanguageNavigation(ChangelanguageNavigationEvent $event): void
+    {
+        if (!isset($this->queryBuilderFactory)) {
+            return;
+        }
+
+        if ($event->getNavigationItem()->isCurrentPage()) {
+            return;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+        if (!($attributes = $request->attributes->get('flare_reader'))) {
+            return;
+        }
+
+        $table = $attributes['table'];
+        $listModel = ListModel::findByPk((int)$attributes['list_id']);
+        if (!$listModel || $listModel->dc !== $table) {
+            return;
+        }
+
+        Controller::loadDataContainer($table);
+        if (($GLOBALS['TL_DCA']['tl_news']['config']['dataContainer'] ?? '') !== Driver::class) {
+            return;
+        }
+
+        $aliasColumnName = $listModel->getAutoItemField();
+        $row = $this->createQueryBuilder($table, $GLOBALS['TL_LANGUAGE'])
+            ->andWhere("($table.$aliasColumnName = :autoitem OR translation.$aliasColumnName = :autoitem)")
+            ->setParameter('autoitem', $request->attributes->get('auto_item'))
+            ->executeQuery()->fetchAssociative();
+
+        if (false === $row) {
+            return;
+        }
+
+        $pidColumn = DcMultilingualHelper::getPidColumn($table);
+        $id = $row[$pidColumn] > 0 ? $row[$pidColumn] : $row['id'];
+
+        $targetRoot = $event->getNavigationItem()->getRootPage();
+        $language = $targetRoot->rootLanguage;
+
+        $fallbackLang = DcMultilingualHelper::getFallbackLanguage($table);
+        if (null !== $fallbackLang && $fallbackLang === $language) {
+            $language = '';
+        }
+
+        $translated = $this->createQueryBuilder($table, $language)
+            ->andWhere("$table.id = :id OR translation.langPid = :id")
+            ->setParameter('id', $id)
+            ->executeQuery()->fetchAssociative();
+
+        if (false === $translated) {
+            return;
+        }
+
+        $event->getUrlParameterBag()->setUrlAttribute('auto_item', $translated['alias'] ?? false);
+    }
+
+    public function setFactory(MultilingualQueryBuilderFactoryInterface $queryBuilderFactory): void
+    {
+        $this->queryBuilderFactory = $queryBuilderFactory;
+    }
+
+    private function createQueryBuilder(string $table, string $language): QueryBuilder
+    {
+        return $this->queryBuilderFactory->build(
+            $table,
+            DcMultilingualHelper::getPidColumn($table),
+            DcMultilingualHelper::getLangColumn($table),
+            DcMultilingualHelper::getRegularFields($table),
+            DcMultilingualHelper::getTranslatableFields($table)
+        )->buildQueryBuilderForFind($language);
     }
 }

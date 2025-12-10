9b3b7439bc75df309ad1cfbb9fc215b052fd7c30
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-12-10 12:05:32 +0100

    Refactor `AbstractListItemProvider` to remove outdated documentation and improve clarity

--- src/ListItemProvider/AbstractListItemProvider.php
+++ src/ListItemProvider/AbstractListItemProvider.php
@@ -7,8 +7,6 @@ use Symfony\Contracts\Service\ServiceSubscriberTrait;
 
 /**
  * Abstract class for list item providers.
- *
- * This class provides a default implementation for {@see ListItemProvider::fetchEntry()} that caches the results.
  */
 abstract class AbstractListItemProvider implements ListItemProviderInterface, ServiceSubscriberInterface
 {

a86632314ee0530ef2d1cd96bd3dabdbb9230300
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-12-11 16:37:46 +0100

    Refactor `AbstractFilterElement` to enhance form options handling and improve documentation clarity; update `SearchKeywordsElement` to use `getFormData()` instead of `getSubmittedData()`

--- src/FilterElement/AbstractFilterElement.php
+++ src/FilterElement/AbstractFilterElement.php
@@ -10,19 +10,37 @@ use HeimrichHannot\FlareBundle\Filter\FilterContext;
 use HeimrichHannot\FlareBundle\Filter\FilterDefinition;
 use HeimrichHannot\FlareBundle\Form\ChoicesBuilder;
 
+/**
+ * @phpstan-template FormOptionsShape of array{
+ *      expanded?: bool,
+ *      label?: string,
+ *      multiple?: bool,
+ *      placeholder?: string
+ *  }
+ */
 abstract class AbstractFilterElement implements FormTypeOptionsContract, IsSupportedContract, PaletteContract
 {
     /**
+     * @var FormOptionsShape|string[] Defines which filter-model fields to use for auto-generating form type options.
+     */
+    public static array $autoFormOptionsMap = [
+        'multiple' => 'isMultiple',
+        'expanded' => 'isExpanded',
+        'label' => 'label',
+        'placeholder' => 'placeholder',
+    ];
+
+    /**
      * Creates default form type options based on default filter model fields and the given config.
      *
-     * @param FilterContext $context
-     * @param array{
-     *     multiple?: bool,
-     *     expanded?: bool,
-     *     placeholder?: bool,
-     * } $config
+     * @param FilterContext $context The filter context.
+     * @param array<string, mixed>|array<int, string>|array<int|string, mixed> $config The config to use.
+     * @phpstan-param FormOptionsShape|list<key-of<FormOptionsShape>> $config
+     *
      * @return array
+     *
      * @api Creates default form type options based on default filter model fields and the given config.
+     * @example $config = ['label', 'multiple', 'placeholder' => 'Select a value']
      */
     public function defaultFormTypeOptions(
         FilterContext $context,
@@ -32,16 +50,27 @@ abstract class AbstractFilterElement implements FormTypeOptionsContract, IsSuppo
 
         $options = [];
 
-        if ($config['multiple'] ?? false) {
-            $options['multiple'] = $filterModel->isMultiple;
-        }
+        /** @var array<string, true> $listPart */
+        $listPart = \array_filter($config, '\is_int', \ARRAY_FILTER_USE_KEY);
 
-        if ($config['expanded'] ?? false) {
-            $options['expanded'] = $filterModel->isExpanded;
-        }
+        foreach (self::$autoFormOptionsMap as $optionName => $attribute)
+        {
+            if (\array_key_exists($optionName, $config))
+            {
+                $value = $filterModel->{$attribute};
 
-        if ($config['placeholder'] ?? false) {
-            $options['placeholder'] = $filterModel->placeholder ?: 'empty_option.select';
+                if ($value === '') {
+                    $value = $config[$optionName];
+                }
+
+                $options[$optionName] = $value ?? $config[$optionName];
+                continue;
+            }
+
+            if (\in_array($optionName, $listPart, true))
+            {
+                $options[$optionName] = $filterModel->{$attribute};
+            }
         }
 
         return $options;

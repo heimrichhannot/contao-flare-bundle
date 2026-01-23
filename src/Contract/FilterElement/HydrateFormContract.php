557291d1b959e1db63071e4f1a9e596bfc960f89
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-01-23 11:53:16 +0100

    refactor: reorder parameters in hydrateForm method for improved clarity

--- src/Contract/FilterElement/HydrateFormContract.php
+++ src/Contract/FilterElement/HydrateFormContract.php
@@ -2,12 +2,11 @@
 
 namespace HeimrichHannot\FlareBundle\Contract\FilterElement;
 
-use HeimrichHannot\FlareBundle\Filter\FilterContext;
 use HeimrichHannot\FlareBundle\Filter\FilterDefinition;
 use HeimrichHannot\FlareBundle\List\ListDefinition;
 use Symfony\Component\Form\FormInterface;
 
 interface HydrateFormContract
 {
-    public function hydrateForm(ListDefinition $list, FilterDefinition $filter, FormInterface $field): void;
+    public function hydrateForm(FormInterface $field, ListDefinition $list, FilterDefinition $filter): void;
 }

7bf53d9c03ceabc2139fd3c0f6bb2e9063eefe6e
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-02-20 13:49:46 +0100

    refactor: reorder method definitions in Env utility class for improved readability

--- src/Util/Env.php
+++ src/Util/Env.php
@@ -6,12 +6,12 @@ use Composer\InstalledVersions;
 use Composer\Semver\VersionParser;
 
 /**
- * @method static bool isContao4()
- * @method static bool isContao5()
  * @method static bool hasContaoCalendar()
  * @method static bool hasContaoComments()
  * @method static bool hasContaoNews()
  * @method static bool hasTerminal42ChangeLanguage()
+ * @method static bool isContao4()
+ * @method static bool isContao5()
  */
 final class Env
 {
@@ -26,12 +26,12 @@ final class Env
         try
         {
             return self::$cache[$name] = match ($name) {
-                'isContao4' => InstalledVersions::satisfies(new VersionParser(), 'contao/core-bundle', '^4.0'),
-                'isContao5' => InstalledVersions::satisfies(new VersionParser(), 'contao/core-bundle', '^5.0'),
                 'hasContaoCalendar' => InstalledVersions::isInstalled('contao/calendar-bundle'),
                 'hasContaoComments' => InstalledVersions::isInstalled('contao/comments-bundle'),
                 'hasContaoNews' => InstalledVersions::isInstalled('contao/news-bundle'),
                 'hasTerminal42ChangeLanguage' => InstalledVersions::isInstalled('terminal42/contao-changelanguage'),
+                'isContao4' => InstalledVersions::satisfies(new VersionParser(), 'contao/core-bundle', '^4.0'),
+                'isContao5' => InstalledVersions::satisfies(new VersionParser(), 'contao/core-bundle', '^5.0'),
                 default => false,
             };
         }

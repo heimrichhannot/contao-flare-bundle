2f821a786313421debbd0ac68dfe1a40513eacef
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-04-28 16:44:28 +0200

    Refactor: Introduce `SimpleEquationFilterType` and `AbstractFilterType` to modularize filter handling

---
+++ src/FilterType/FilterTypeInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace HeimrichHannot\FlareBundle\FilterType;
+
+use HeimrichHannot\FlareBundle\Query\FilterQueryBuilder;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+interface FilterTypeInterface
+{
+    /**
+     * Configures the options for this type.
+     */
+    public function configureOptions(OptionsResolver $resolver): void;
+
+    /**
+     * Builds the filter query.
+     *
+     * @param array<string, mixed> $options
+     */
+    public function buildQuery(FilterQueryBuilder $builder, array $options): void;
+}

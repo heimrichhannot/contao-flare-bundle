f6664e21d0e156ea3a4613b1411f91f2e78745f1
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-04-28 13:45:26 +0200

    Mark exception classes as internal.

--- src/Exception/FlareException.php
+++ src/Exception/FlareException.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace HeimrichHannot\FlareBundle\Exception;
 
+/**
+ * @internal This exception is thrown internally and should not be
+ *           thrown by userland code. You may catch it, however.
+ */
 class FlareException extends \Exception
 {
     public function __construct(

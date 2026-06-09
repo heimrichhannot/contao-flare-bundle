172502ae0a699a46e3b67024288c4cde35674eab
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-06-09 15:08:42 +0200

    Update FilterFormFactory and flare.js to enhance query handling

--- public/frontend/flare.js
+++ public/frontend/flare.js
@@ -1,12 +1,13 @@
-((d, a, b, l) => {
-    b = () => d.querySelectorAll(`form${a}, ${a} form`).forEach(f => {
+((d, s, q, b, l) => {
+    b = () => d.querySelectorAll(`form${s}, ${s} form`).forEach(f => {
         if (f.method?.toUpperCase?.() !== 'GET') return;
         l = add => (e => {
             if (!f.action || !f.name) return;
             e.preventDefault();
+            const n = Array.from(f.querySelectorAll(q) || []).map(e => e.name);
             const url = new URL(f.action), del = [];
             for (const k of url.searchParams.keys()) {
-                if (k.startsWith(f.name + '[')) del.push(k);
+                if (k.startsWith(f.name + '[') || n.includes(k)) del.push(k);
             }
             del.forEach(key => url.searchParams.delete(key));
             add ? new FormData(f).forEach((v, k) => {
@@ -18,4 +19,4 @@
         f.addEventListener('reset', l(0));
     });
     d.readyState === 'loading' ? d.addEventListener('DOMContentLoaded', b) : b();
-})(document, '[data-flare-keep-query]');
+})(document, '[data-flare-form="keep-query"]', '[data-flare-form="query-field"]');

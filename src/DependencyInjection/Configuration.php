169224c54b635ba29978f6ea51b647cfc9a5593d
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-07-14 17:01:55 +0200

    add clarification comment for PHPStan ignore annotation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,7 +14,9 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('huh_flare');
         $rootNode = $treeBuilder->getRootNode();
 
-        // @phpstan-ignore class.notFound (PHPStan 1.x cannot parse symfony/config 7.4 template defaults)
+        // PHPStan 1.x cannot parse symfony/config 7.4 template defaults.
+        // This phpstan-ignore annotation is only required when using symfony/config >= 7:
+        // @ ### phpstan-ignore class.notFound
         $rootNode
             ->children()
                 ->arrayNode('format_label_defaults')
